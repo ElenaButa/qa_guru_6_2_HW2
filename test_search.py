8bc0c259c8ce439e7ec5c9b02d958c549ca15b5a
Elena Butakova <84374901+ElenaButa@users.noreply.github.com>
2023-06-14 17:27:27 +0500

    Update test_search.py

--- test_search.py
+++ test_search.py
@@ -10,13 +10,13 @@ def set_size():
     browser.config.window_height = 740
 
     
-def test_success_search(set_size, site):  # Check result of search
+def test_success_search(set_size):  # Check result of search
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_unsuccess_results(set_window_size, site):  # Check message "unsuccess search"
+def test_unsuccess_results(set_window_size):  # Check message "unsuccess search"
     browser.open('https://google.com')
     textUnsuccess = 'янапотаповазвездаророолоролоролоролорлорлорлорлололлор'
     browser.element('[name="q"]').should(be.blank).type(textUnsuccess).press_enter()
